37485c95097e801c31b8cda6b894cd45f85df8ce
saldhyyoga <saldhyy@gmail.com>
2019-12-12 14:42:33 +0700

    add register page

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,58 +10,64 @@ import Login from "./Login";
 export default class Register extends Component {
   render() {
     return (
-      <div className="App-content">
-        <Typography component="div">
-          <Box className="Title" mt={3}>
-            SIGN UP
+      <div className="FormRegister">
+        <div className="BoxRegister">
+          <Typography component="div">
+            <center>
+              <div style={{ fontSize: "32px" }}>Sign in with Email</div>
+            </center>
+            <Box mt={1} textAlign="center">
+              create an account to receive great medium stories in your inbox
+              <br />
+              personalize your homepage, and follow authors
+              <br />
+              and topics that you love.
+            </Box>
+          </Typography>
+          <Box textAlign="center" mt={2}>
+            <TextField
+              placeholder="Enter username or email"
+              className="SizeTextField"
+              helperText="example saldhyyoga or saldhyy@gmail.com"
+            />
           </Box>
-          <Box mt={1} textAlign="center">
-            create an account to receive great medium stories in your inbox
-            <br />
-            personalize your homepag, and follow authors
-            <br />
-            and topics that you love.
+          <Box textAlign="center" mt={2.5}>
+            <TextField
+              className="SizeTextField"
+              placeholder="Enter password"
+              size="large"
+              type="password"
+            />
           </Box>
-        </Typography>
-        <Box textAlign="center" mt={2}>
-          <TextField
-            placeholder="Enter username or email"
-            className="SizeTextField"
-            helperText="example saldhyyoga or saldhyy@gmail.com"
-          />
-        </Box>
-        <Box textAlign="center" mt={2.5}>
-          <TextField
-            className="SizeTextField"
-            placeholder="Enter password"
-            size="large"
-            type="password"
-          />
-        </Box>
-        <Box textAlign="center" mt={3}>
-          <Button className="Button-style">
+          <Box textAlign="center" mt={3}>
+            <Button className="Button-style">
+              <Link
+                to="/homepage"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                Register
+              </Link>
+            </Button>
+          </Box>
+          <Box textAlign="center" mt={3}>
+            Already have an account ?{" "}
             <Link
-              to="/homepage"
-              style={{ textDecoration: "none", color: "white" }}
+              to="/login"
+              style={{ color: "#12af92", textDecoration: "none" }}
             >
-              Register
+              Signin
             </Link>
-          </Button>
-        </Box>
-        <Box textAlign="center" mt={3}>
-          Already have an account ?{" "}
-          <Link
-            to="/login"
-            style={{ color: "#12af92", textDecoration: "none" }}
-          >
-            Signin
-          </Link>
-        </Box>
-        <Box textAlign="center" mt={2}>
-          To make Medium work,we log user data and share it with service
-        </Box>
-        <Box textAlign="center">Providers. Click "Sign Up" above to accept</Box>
-        <Box textAlign="center">Medium's Term of Service & Privacy Policy</Box>
+          </Box>
+          <Box textAlign="center" mt={2}>
+            To make Medium work,we log user data and share it with service
+          </Box>
+          <Box textAlign="center">
+            Providers. Click "Sign Up" above to accept
+          </Box>
+          <Box textAlign="center">
+            Medium's Term of Service & Privacy Policy
+          </Box>
+        </div>
       </div>
     );
   }
